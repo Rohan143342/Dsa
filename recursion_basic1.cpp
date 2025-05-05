2baae9fb000f0bcf8ac91bfc8d71d8dea96026d3
Rohan143342 <136481783+Rohan143342@users.noreply.github.com>
2025-05-05 23:14:06 +0530

    Add files via upload

---
+++ recursion_basic1.cpp
@@ -0,0 +1,9 @@
+//this is the day 1 of learning recursion in cpp
+#include <bits/stdc++.h>
+void functioncalling(){
+    printf("1");
+    functioncalling();
+};
+int main(){
+    functioncalling();
+}
